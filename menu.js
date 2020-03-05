79a35b064a8ede73ceed196b6e57bafe67569035
Yqloss <yqloss@yeah.net>
2020-03-05 13:41:34 +0800

    0

--- menu.js
+++ menu.js
@@ -1,2 +1,2 @@
 var menu=document.getElementById("menu");
-menu.innerHTML="<a href=\"https://yqloss.github.io\">Go back</a><br><b>----Menu----</b><br><a href=\"https://yqloss.github.io/Applications/0.html\">Syobon Action</a><br><a href=\"https://yqloss.github.io/Applications/1.html\">Visual C++ 6.0</a><br><a href=\"https://yqloss.github.io/Applications/2.html\">CChess</a>"
+menu.innerHTML="<a href=\"https://yqloss.github.io\">Go back</a><br><b>----Menu----</b><br><a href=\"https://yqloss.github.io/Applications/0.html\">Syobon Action</a><br><a href=\"https://yqloss.github.io/Applications/1.html\">Visual C++ 6.0</a><br><a href=\"https://yqloss.github.io/Applications/2.html\">CChess</a><br><a href=\"https://yqloss.github.io/Applications/3.html\">NBT Explorer</a>"
